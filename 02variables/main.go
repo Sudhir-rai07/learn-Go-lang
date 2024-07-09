8c0d3f3db74058b52f0350b3d808815369a5022d
Sudhir Rai <sudhirrai128@gmail.com>
2024-07-09 09:02:21 +0530

    datatypes and variables in golang

---
+++ 02variables/main.go
@@ -0,0 +1,20 @@
+package main
+
+import "fmt"
+
+func main() {
+
+	fmt.Println("Welcome to variables")
+
+	// Variable decleration types
+
+	// var name string = "Google" // Type one
+	// name := "Google" // Type two
+	// fmt.Print("Who created golang: ", name)
+
+	// Datatypes in go
+	// int 7 8 90
+	// float  9.11 26.11
+	// string "Nancy" "Johncena"
+	// boolean true false
+}
